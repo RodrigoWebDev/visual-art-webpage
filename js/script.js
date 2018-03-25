c02a47aea50c280ae6d5cade5d6cbebafc4f6a3b
Rodrigo Queiroz <rodrigo.queiroz.chagas@gmail.com>
2018-03-25 02:29:10 -0300

    Imagens otimizadas, js minificado, e css prefixado

--- js/script.js
+++ js/script.js
@@ -6,28 +6,28 @@ $(function () {
             let svg = $('nav i');
             svg.removeClass('icon-arrow-up');
             svg.addClass('icon-arrow-down');
-        }
+        };
         if ($('.drop-menu').is(':hidden')) {
             let svg = $('nav i');
             svg.removeClass('icon-arrow-down');
             svg.addClass('icon-arrow-up');
-        }
+        };
         $(".drop-menu").toggle(400);
 
-    })
+    });
 
     //Lightbox
     $(".close").click(function(){
         $(".overlay").hide(500);
-    })
+    });
     
     $(".work img").click(function(){
-        $(".overlay img").attr("src",$(this).attr("src"))
+        $(".overlay img").attr("src",$(this).attr("src"));
         $(".overlay").show(500);
-    })
+    });
 
 
 
 
 
-})
+});
